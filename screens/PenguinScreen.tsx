099d459e41d7307d424c0992b6a3f4a6f786c0e9
David Schmid <davethehacker@users.noreply.github.com>
2020-09-19 17:42:30 +0200

    add graph

--- screens/PenguinScreen.tsx
+++ screens/PenguinScreen.tsx
@@ -2,6 +2,31 @@ import * as React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import styled from "styled-components";
+import { LineChart } from "react-native-chart-kit";
+import { Dimensions } from "react-native";
+
+const screenWidth = Dimensions.get("window").width;
+
+const data = {
+  labels: ["January", "February", "March", "April", "May", "June"],
+  datasets: [
+    {
+      data: [11.5, 10.4, 9, 8.5, 7.3, 6.6],
+      color: (opacity = 1) => `rgba(0, 153, 255, ${opacity})`, // optional
+      strokeWidth: 5 // optional
+    }
+  ],
+  //legend: ["Rainy Days"] // optional
+};
+
+const chartConfig = {
+  backgroundColor: "white",
+  backgroundGradientFrom: "white",
+  backgroundGradientTo: "white",
+  color: (opacity = 1) => `rgba(0, 153, 255, ${opacity})`,
+  //barPercentage: 0.5,
+  useShadowColorFromDataset: false // optional
+};
 
 const ScreenView = styled(SafeAreaView)`
   position: relative;
@@ -24,8 +49,18 @@ const Seperator = styled(View)`
 export default function TabPenguin() {
   return (
     <ScreenView>
+      <LineChart
+        data={data}
+        width={screenWidth}
+        height={256}
+        verticalLabelRotation={30}
+        chartConfig={chartConfig}
+        bezier
+        withDots={false}
+      />
       <StyledText>Tab Penguin</StyledText>
       <Seperator />
+      
     </ScreenView>
   );
 }
